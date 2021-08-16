8ed779ae956f112d7e7ecaf90c29b9d0e9aa1e4b
nnsncl <nuniroland@gmail.com>
2021-08-16 16:18:26 +0200

    Update Brand

--- app/src/components/Navigation/Navigation.js
+++ app/src/components/Navigation/Navigation.js
@@ -3,15 +3,7 @@ import React from 'react';
 export default function Navigation() {
     return (
         <nav className='text-white bg-gray-800 py-12 px-2 w-1/12 h-screen flex-col flex items-center justify-between' >
-            <span>
-                <svg height="40" viewBox="0 0 77 84" fill="none" >
-                    <path d="M36.8903 4.39471L71.9299 21.5L37.9386 39.2631L4.16675 22.5964L36.8903 4.39471Z" fill="#131517" stroke="white" strokeWidth="7" strokeLinejoin="round" />
-                    <path d="M3.50879 63.6053V23.0351L38.1579 40.579V81.1491L3.50879 63.6053Z" fill="#131517" stroke="white" strokeWidth="7" strokeLinejoin="round" />
-                    <path d="M38.158 81.1491V40.7982L72.5878 22.1579V62.5088L38.158 81.1491Z" fill="#131517" stroke="white" strokeWidth="7" strokeLinejoin="round" />
-                    <path fillRule="evenodd" clipRule="evenodd" d="M38.0434 56.9803L35.4373 61.9557L3.20044 45.0697L6.44849 38.8689L38.3276 55.5675L69.7392 37.4043L73.2432 43.4642L41.0064 62.1045L38.0434 56.9803Z" fill="white" />
-                    <path d="M50.8772 60.3158V47.1153L62.2807 41.0175V54.218L50.8772 60.3158Z" fill="#131517" stroke="white" strokeWidth="7" strokeLinejoin="round" />
-                </svg>
-            </span>
+            <span className='text-sm' >Bit<b>Chest</b></span>
             <ul className='flex flex-col h-full'>
                 <li className='mt-16 text-center' >
                     <svg width="24" height="25" viewBox="0 0 24 25" fill="none" >
