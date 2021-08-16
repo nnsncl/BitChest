c1640bf9e10139b08f16eeeab05788fd6b5c654f
nnsncl <nuniroland@gmail.com>
2021-08-16 15:30:28 +0200

    Update tailwind color theme

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      black: '#111111',
+      white: '#fff',
+      gray: {
+        700: '#787a7b',
+        800: '#1E1F23',
+        900: '#131517'
+      }
+    }
   },
   variants: {
     extend: {},
