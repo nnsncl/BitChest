8df44b90305d3a97c5a55d3dab65206f8d621f5e
nnsncl <nuniroland@gmail.com>
2021-08-16 21:25:54 +0200

    Brand class

--- app/src/pages/Wallet.js
+++ app/src/pages/Wallet.js
@@ -5,8 +5,13 @@ import Navigation from '../components/Navigation';
 
 export default function Wallet() {
     return (
-        <main className='bg-gray-900 md:flex h-screen p-6' >
+        <main className='bg-gray-900 text-white md:flex h-screen p-6' >
             <Navigation />
+            <section className='md:py-10 py-9 md:px-12 px-6'>
+                <h1 className='gradient-text text-3xl font-bold' >Dashboard</h1>
+                <p className='text-gray-700' >Cryptocurrencies management</p>
+            </section>
+
             {/* <Table /> */}
         </main>
     );
