b65ce7fca162989c752ba083af66949b8e83e747
nnsncl <nuniroland@gmail.com>
2021-08-16 14:02:04 +0200

    update routes names

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router,  Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import * as ROUTES from './constants/routes'
 
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <Router>
       <Switch>
-        <Route path={ ROUTES.ADMIN }>
+        <Route path={ROUTES.ADMIN}>
           Wallet
         </Route>
         <Route path={ROUTES.HOME}>
--- app/src/constants/routes.js
+++ app/src/constants/routes.js
@@ -1,2 +1,2 @@
 export const HOME = '/'
-export const ADMIN = '/admin'
+export const ADMIN = '/wallet'
