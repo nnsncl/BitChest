7c11bb187b381f332e4e68505aba42ecaaffff39
nnsncl <nuniroland@gmail.com>
2021-08-16 23:31:58 +0200

    Animate ButtonGhost

--- app/src/components/Button.js
+++ app/src/components/Button.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import { motion } from 'framer-motion';
 
-export const Button = ({ to, children }) => {
+export const ButtonLink = ({ to, children }) => {
     return (
         <Link to={to} className='p-3 rounded-lg transition duration-300 ease-in-out'>
             {children}
         </Link>
     );
 };
+
+export const ButtonGhost = ({ onClick, children }) => {
+
+    return (
+        <motion.button
+            whileHover={{
+                scale: 1.2,
+                rotate: '90deg'
+            }}
+            whileTap={{
+                scale: 0.9,
+                rotate: '-90deg',
+                borderRadius: '100%',
+                transition: {
+                    type: "spring",
+                    mass: 2
+                }
+            }}
+            className='px-4 py-2 border-2 border-gray-800 rounded-xl font-bold'
+            onClick={onClick}>
+            {children}
+        </motion.button>
+    );
+}
