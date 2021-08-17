ed1a0f949d9471c78deed973f3e99c479279e301
Raze91 <hattab_sami@yahoo.fr>
2021-08-17 09:55:09 +0200

    add a tableHeads constant file

--- app/src/pages/Admin/GetUsers.js
+++ app/src/pages/Admin/GetUsers.js
@@ -4,32 +4,23 @@ import Navigation from "../../components/Navigation/Navigation";
 import Table from "../../components/Table/Table";
 import TableRow from "../../components/Table/TableRow/TableRow";
 
+import { column_titles, userCryptoData } from "../../constants/tableHeads";
+
 export default function GetUsers() {
-
-    const column_titles = [
-        "Coin",
-        "Price",
-        "1h",
-        "24 volume",
-        "Market Cap",
-        "Last 7 Days"
-    ];
-
-    const userCryptoData = [
-        "BTC/USDT",
-        "$35.608,12",
-        "+0.8%",
-        "$26,473,275,010",
-        "$635,353,344,920",
-        "graph"
-    ];
 
     return (
         <main className='bg-gray-900 h-screen flex' >
             <Navigation />
 
             <Table columnTitles={column_titles} data={userCryptoData}>
-                <TableRow icon={"icon"} data={userCryptoData} error />
+                <TableRow icon={"icon"} data={userCryptoData} error>
+                    <td>
+                        <button>Edit</button>
+                    </td>
+                    <td>
+                        <button>Delete</button>
+                    </td>
+                </TableRow>
             </Table>
         </main>
     )
