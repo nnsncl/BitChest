32eb35bc7cc454ca61aff7409eb61b585cc60476
nnsncl <nuniroland@gmail.com>
2021-08-17 11:29:58 +0200

    create coin context

--- app/src/index.js
+++ app/src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { CoinsProvider } from './hooks/use-currencies';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CoinsProvider>
+      <App />
+    </CoinsProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
