28cabdf3e947a1bbc558368d1464a11a362a5284
Raze91 <hattab_sami@yahoo.fr>
2021-08-19 14:27:21 +0200

    add the currencies api routes

--- server/routes/api.php
+++ server/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CurrenciesController;
 use App\Http\Controllers\TransactionsController;
 use App\Http\Controllers\UsersController;
 
@@ -27,12 +28,17 @@ Route::get('/transaction/{id}', [TransactionsController::class, 'show']);
 Route::post('/transactions', [TransactionsController::class, 'store']);
 Route::delete('/transaction/{id}', [TransactionsController::class, 'destroy']);
 
+Route::get('/currencies', [CurrenciesController::class, 'index']);
+Route::get('/currency/{id}', [CurrenciesController::class, 'show']);
+Route::post('/currency', [CurrenciesController::class, 'store']);
+Route::put('/currency/{id}', [CurrenciesController::class, 'update']);
+
 // Auth routes
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
 // Protected Routes
-Route::group(['middleware' => ['auth:sanctum']], function() {
+Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 
     Route::get('/users', [UsersController::class, 'index']);
