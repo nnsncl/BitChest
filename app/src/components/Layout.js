191fc19395ac4ae9a8993286773567b277e15c81
nnsncl <nuniroland@gmail.com>
2021-08-25 21:32:21 +0200

    Remove balance from pages when not needed

--- app/src/components/Layout.js
+++ app/src/components/Layout.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 
-import { useAuth } from '../hooks/use-auth';
-
 import { Banner } from './Banner';
 import { Navigation } from './Navigation';
-import { Swap } from './Icons';
 
 import { container } from '../animations/motion';
 
 export const Layout = ({ children }) => {
-    const auth = useAuth();
 
     return (
         <>
@@ -28,12 +24,6 @@ export const Layout = ({ children }) => {
                     <p className='text-sm text-white'>©&nbsp;Bit<b>Chest</b>&nbsp;2021</p>
                     <p className='text-xs '>Take a 👀 at the <a className='font-bold text-white' href='https://github.com/nnsncl/BitChest' target='_blank' rel="noreferrer">source code</a></p>
                 </footer>
-                {auth.user &&
-                    <button className='flex items-center gap-2 bg-blue-900 text-sm py-3 px-6 rounded-lg fixed bottom-6 right-6' >
-                        <Swap />
-                        {auth.user.balance}€
-                    </button>
-                }
             </main>
         </>
     );
