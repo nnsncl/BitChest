d55d9bffe338253381f222d2f55267e348fef81c
Raze91 <hattab_sami@yahoo.fr>
2021-08-28 22:18:26 +0200

    create a seeder for the currency table / add tests for http requests

---
+++ server/database/seeders/CurrencyTableSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Http\Client\Pool;
+
+class CurrencyTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        $GET_BTC_SET= 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&ids=bitcoin&order=market_cap_desc&per_page=10&page=1&sparkline=false';
+        $GET_ETH_SET= 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&ids=ethereum&order=market_cap_desc&per_page=10&page=1&sparkline=false';
+        $GET_XRP_SET= 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&ids=ripple&order=market_cap_desc&per_page=10&page=1&sparkline=false';
+        $GET_BCH_SET= 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&ids=bitcoin-cash&order=market_cap_desc&per_page=10&page=1&sparkline=false';
+        $GET_ADA_SET= 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&ids=cardano&order=market_cap_desc&per_page=10&page=1&sparkline=false';
+        $GET_LTC_SET= 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&ids=litecoin&order=market_cap_desc&per_page=10&page=1&sparkline=false';
+        $GET_XEM_SET= 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&ids=nem&order=market_cap_desc&per_page=10&page=1&sparkline=false';
+        $GET_XLM_SET= 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&ids=stellar&order=market_cap_desc&per_page=10&page=1&sparkline=false';
+        $GET_MIOTA_SET= 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&ids=iota&order=market_cap_desc&per_page=10&page=1&sparkline=false';
+        $GET_DASH_SET= 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&ids=dash&order=market_cap_desc&per_page=10&page=1&sparkline=false';
+
+        $requestsArray = [$GET_BTC_SET, $GET_ETH_SET, $GET_XRP_SET, $GET_BCH_SET, $GET_ADA_SET, 
+        $GET_LTC_SET, $GET_XEM_SET, $GET_XLM_SET, $GET_MIOTA_SET, $GET_DASH_SET];
+
+        // $response = Http::get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&ids=bitcoin&order=market_cap_desc&per_page=10&page=1&sparkline=false');
+
+        // $object = $response->object();
+
+        $returnValue = function ($value) {
+            return $value;
+        };
+
+        $responses = array_map($returnValue, $requestsArray);
+
+        dd($responses);
+    }
+}
