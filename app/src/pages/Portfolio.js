faa9a2347ed71139eea9a5ab91f6b8897dab7d86
Raze91 <hattab_sami@yahoo.fr>
2021-09-01 17:46:19 +0200

    add the user's transactions table, possessed currencies table in progress

--- app/src/pages/Portfolio.js
+++ app/src/pages/Portfolio.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
 import { useAuth } from '../hooks/use-auth';
@@ -11,17 +11,25 @@ import { GraphUp } from '../components/Icons';
 import { TransactionsModule } from '../components/TransactionsModule';
 
 import { container, article } from '../animations/motion';
+import moment from 'moment';
 
 
 export default function Portfolio() {
     const auth = useAuth();
-    const transactions = useContext(TransactionsContext);
+    const { transactions}  = useContext(TransactionsContext);
     const { refs, storedCoins } = useContext(CoinsContext);
 
     const [totalCoinsTableVisible, setTotalCoinsTableVisible] = useState(true);
+    const [userCoins, setUserCoins] = useState([]);
 
-    console.log(refs)
-    console.log(transactions)
+    useEffect(() => {
+        transactions.map((item, key) => {
+            if(item.currency_id === refs[key].id && refs[key].coin_id === storedCoins[key].coin_id) {
+                setUserCoins([...userCoins, refs[key]])
+                console.log(refs[key])
+            }
+        })
+    }, []);
 
     return (
         <Layout>
@@ -52,7 +60,7 @@ export default function Portfolio() {
                             <th className="w-1/4 text-right text-xs" >Total Coin</th>
                         </>
                     }>
-                        {(storedCoins && totalCoinsTableVisible) &&
+                        {(userCoins && totalCoinsTableVisible) &&
                             storedCoins.map((item, key) => (
                                 <motion.tr
                                     key={key}
@@ -67,32 +75,51 @@ export default function Portfolio() {
                                             <span className='text-gray-700 text-xs uppercase'>{item.symbol}</span>
                                         </p>
                                     </motion.td>
-                                    <motion.td variants={article} className='w-1/4 flex items-cente justify-end gap-3' >
-                                        <p className={`${item.price_change_percentage_24h >= 0 ? 'text-green-900' : 'text-red-900'} text-right flex items-center text-sm uppercase`} >
-                                            {item.price_change_percentage_24h >= 0
-                                                ? <small>&#x25B2;</small>
-                                                : <small>&#x25BC;</small>
-                                            }
-                                            &nbsp;{item.current_price.toLocaleString()}€
-                                        </p>
-                                    </motion.td>
-                                    <motion.td variants={article} className='w-1/4 flex items-cente justify-end gap-3' >
-                                        <p className={`${item.price_change_percentage_24h >= 0 ? 'text-green-900' : 'text-red-900'} text-sm flex gap-1 text-right`} >
-                                            {item.price_change_percentage_24h >= 0
-                                                ? <>
-                                                    <small>&#x25B2;</small>
-                                                    {item.price_change_percentage_24h.toFixed(2)}%
-                                                </>
-                                                : <>
-                                                    <small>&#x25BC;</small>
-                                                    {item.price_change_percentage_24h.toFixed(2) * -1}%
-                                                </>
-                                            }
-                                        </p>
-                                    </motion.td>
                                 </motion.tr>
                             ))
                         }
+                    </Table>
+                    <div className='flex items-center justify-between gap-6 mb-3' >
+                        <h3 className='text-base font-light'>Recent Activity</h3>
+                        <button
+                            onClick={() => setTotalCoinsTableVisible(!totalCoinsTableVisible)}
+                            className='flex items-center gap-3 font-light text-xs py-2 px-4 border-2 border-gray-800 hover:bg-gray-800 rounded-full outline-none' >
+                            See all
+                        </button>
+                    </div>
+                    <Table>
+                        {transactions.map((item, key) => (
+                                <motion.tr 
+                                    className='rounded-b-2xl flex items-center justify-between gap-6 text-white py-6 px-4 gap-6 border-t-2 border-gray-800'
+                                    key={key}
+                                >
+                                    <motion.td className='w-1/7 flex flex-col items-center justify-start uppercase'>
+                                        <p className="text-gray-700">
+                                            {moment(item.created_at).format("MMM")}
+                                        </p>
+                                        <p>
+                                            {moment(item.created_at).format("DD")}
+                                        </p>
+                                    </motion.td>
+                                    <motion.td className='w-1/7 flex items-center justify-start gap-3'>
+                                        {item.type === 1 ? "Bought" : "Sold"}
+                                    </motion.td>
+                                    <motion.td className='w-2/7 flex items-center justify-start gap-3'>
+                                        {refs.filter(ref => ref.id === item.currency_id)[0].name}
+                                    </motion.td>
+                                    <motion.td className='w-3/7'>
+                                        <p className={`${item.type === 1 ? "text-green-900" : "text-red-900"}`}>
+                                            {item.type === 1 ? "+" : "-"}
+                                            {item.currency_quantity}
+                                        </p>
+                                        <p className="flex justify-end text-gray-700">
+                                            {item.type === 1 ? "-" : "+"}
+                                            {item.transaction_amount}€
+                                        </p>
+                                    </motion.td>
+                                </motion.tr>
+                            )
+                        )}
                     </Table>
                 </div>
                 <div className="md:w-1/4 w-full">
